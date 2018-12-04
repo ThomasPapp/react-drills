626a85bed91f06631cdbe58129d0e20a09693b05
Thomas Papp <pappmthomas@gmail.com>
2018-12-03 19:25:28 -0600

    completed app-1, app-2, and app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      foods: [
+        "spaghetti",
+        "ice cream",
+        "sushi",
+        "bologna",
+        "cheese"
+      ],
+      input: ""
+    }
+  }
+
+  updateInput = (e) => this.setState({ input: e.target.value })
+
   render() {
+    const foods = this.state.foods.filter(food => food.includes(this.state.input)).map(food => <p>{food}</p>)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input value={this.state.input} onChange={this.updateInput} />
+        {foods}
       </div>
     );
   }
